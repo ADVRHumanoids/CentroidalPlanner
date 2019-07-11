43981be221ab50eafabb3c27fef61552ff4fe76c
matteoparigi <matteo.parigi@iit.it>
2019-07-11 13:13:36 +0200

    Utils implementation

---
+++ include/CentroidalPlanner/Utils.h
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+#include <cartesian_interface/CartesianInterfaceImpl.h>
+#include <cartesian_interface/ros/RosServerClass.h>
+#include <cartesian_interface/ros/RosImpl.h>
+#include <Eigen/Geometry> 
+
+namespace cpl { namespace utils { 
+     
+Eigen::Affine3d GetAffineFromNormal(const Eigen::Vector3d& n);
+    
+class SurfaceReacher
+{
+    
+public:
+
+    typedef std::shared_ptr<SurfaceReacher> Ptr;
+    
+    SurfaceReacher(std::vector<std::string> contact_names); 
+    
+    bool ReachSurface(XBot::Cartesian::RosImpl ci,
+                      std::string contact_name, 
+                      const Eigen::Vector3d contact_lin_vel, 
+                      double F_thr);
+    
+private:
+    
+    std::vector<std::string> _contact_names;
+    std::map<std::string, ros::Subscriber> _sub_force_map;
+    std::map<std::string, Eigen::Vector6d> _f_est_map;
+    
+};
+    
+    
+} }
